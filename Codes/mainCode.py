72fca6ed6cf50206d5e0f6e3e66f9a5c10e5b5e5
robertdccaetano <robertvisualmidia@gmail.com>
2019-03-10 18:38:00 -0300

    Atualizando maincode da ferramenta 'Áspide Recognizer' TCC murilorobert

--- Codes/mainCode.py
+++ Codes/mainCode.py
@@ -1,14 +1,19 @@
 import speech_recognition as sr
+from time import sleep as wait
 
-sr.Microphone.list_microphone_names()
+print(sr.Microphone.list_microphone_names())
+
 
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    print('Diga algo: ')
+    r.adjust_for_ambient_noise(source)
+    print('\033[35mOlá! Eu sou seu computador, vamos lá diga alguma coisa para nós conversarmos... \033[m')
     audio = r.listen(source)
     try:
-        text = r.recognize_google(source, language='pt-BR')
-        print('Voce disse: {}'.format(text))
+        text = r.recognize_google(audio, language='pt-BR')
+        print('\033[2;30mCalma aí vou anotar o que você me falou, preciso aprender mais sobre humanos rs =D \033[m')
+        wait(5)
+        print('\033[4;32mEu te entendi, legal! Parece que você quis me dizer, {}?\033[m'.format(text))
     except sr.UnknownValueError:
-        print('Não te entendi me desculpe.')
+        print('\033[31mNão te entendi me desculpe, execute-me novamente!')
