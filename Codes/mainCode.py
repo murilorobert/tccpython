508332dae2699a0a6d3dbf800db19e50af4f0b72
murilorobert <tcmurilorobert@gmail.com>
2019-03-10 17:53:33 -0300

    upp att mainCode

--- Codes/mainCode.py
+++ Codes/mainCode.py
@@ -2,14 +2,13 @@ import speech_recognition as sr
 
 sr.Microphone.list_microphone_names()
 
-'''r = sr.Recognizer()
+r = sr.Recognizer()
 
 with sr.Microphone() as source:
     print('Diga algo: ')
     audio = r.listen(source)
     try:
-        text = r.recognize_google(audio)
+        text = r.recognize_google(source, language='pt-BR')
         print('Voce disse: {}'.format(text))
     except sr.UnknownValueError:
         print('Não te entendi me desculpe.')
-'''
