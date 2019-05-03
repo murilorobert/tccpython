c06bc4d80c4ce1b4835463fbb702baf46e1ebfcc
robertdccaetano <robertvisualmidia@gmail.com>
2019-05-03 10:55:45 -0300

    Upp mainCode renamed for codigoEstruturado 03052019 10:55

--- Codes/codigoEstruturado.py
+++ Codes/codigoEstruturado.py
@@ -1,29 +1,31 @@
 import os
-import speech_recognition as sr
+import speech_recognition as spch
 from time import sleep as wait
 
 os.system('cls' if os.name == 'nt' else 'clear') 
-#print(sr.Microphone.list_microphone_names())
+#print(spch.Microphone.list_microphone_names()) --
 resp = 's'
 while(resp == 's'):
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
+    r = spch.Recognizer()
+    with spch.Microphone() as source:
         r.adjust_for_ambient_noise(source)
         print('\033[35mOlá! Eu sou seu computador, vamos lá diga alguma coisa para nós conversarmos... \033[m')
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio, language='pt-BR')
             print('\033[33mCalma aí vou anotar o que você me falou, primeiro me informe onde quer salvar seu arquivo... \033[m')
-            wait(3)
+            wait(2)
             try:
-                nome_arquivo = input('\033[3;31mDigite o caminho+nome para salvar seu arquivo Ex: /path/nomearquivo.txt:  ')
+                nome_arquivo = input('\033[3;31mDigite caminho/nomearquivo.txt para salvá-lo:     ')
                 arquivo = open(nome_arquivo,  'w')
                 arquivo.write(text)
                 print('\033[4;32mEu te entendi, legal! Parece que você quis me dizer, {}?\033[m'.format(text))       
                 arquivo.close()
+                resp = 'n'
             except FileNotFoundError:
                 print('Caminho incorreto')
-        except sr.UnknownValueError:
+                nome_arquivo = input('Tente novamente: ')
+        except spch.UnknownValueError:
             print('\033[31mNão te entendi me desculpe... :/ ')
-            resp = input('Quer tentar novamente ? [S/N]] ')
+            resp = input('Quer tentar novamente? [S/N]] ')
             os.system('cls' if os.name == 'nt' else 'clear')            
